53ae8ac9cdc01043cac360e33ac15b2006a1326a
Brittany Bilotti <brbi1248@colorado.edu>
2023-03-14 17:02:24 +0000

    Add cat data to PetData directory

--- PetData/.ipynb_checkpoints/petattrributes-checkpoint.sh
+++ PetData/.ipynb_checkpoints/petattrributes-checkpoint.sh
@@ -1,8 +1,10 @@
 #This is a script to scrape each pet html page for attributes: name, breed, age, gender, color, size
-
+#to do: background and location
 if [[ -f $1 ]]; then
     echo -e "Name:" > "$1"_attributes.txt
     grep -o -P '(?<="lbName">).*(?=</span>)' $1 >> "$1"_attributes.txt
+    echo -e "Description: " > "$1"_attributes.txt
+    grep -o -P '(?<="og:description">).*(?="To begin the")' $1 >> "$1"_attributes.txt
     echo -e "Breed:" >> "$1"_attributes.txt
     grep -o -P '(?<="lbBreed">).*(?=</span>)' $1 >> "$1"_attributes.txt
     echo -e "Age:" >> "$1"_attributes.txt
@@ -13,6 +15,8 @@ if [[ -f $1 ]]; then
     grep -o -P '(?<="lblColor">).*(?=</span>)' $1 >> "$1"_attributes.txt
     echo -e "Size:" >> "$1"_attributes.txt
     grep -o -P '(?<="lblSize">).*(?=</span>)' $1 >> "$1"_attributes.txt
+    echo -e "Location:" >> "$1"_attributes.txt
+    grep -o -P '(?<="lblLocation">).*(?=</span>)' $1 >> "$1"_attributes.txt
     
 else
     echo "file does not exist"
