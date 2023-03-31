e4325925d67099d9370e05b7f9a73ff1eafafda1
Brittany Bilotti <brbi1248@colorado.edu>
2023-03-31 21:00:25 +0000

    Created individual cat and dog html pages

--- Furever.py
+++ Furever.py
@@ -35,16 +35,36 @@ def catpage():
 def dogpage():
     return render_template('DogPage.html')
 # 5 ids for dogs page 
-# @app.route('/36636186')
-# @app.route('/42904054')
-# @app.route('/43078721')
-# @app.route('/45447002')
-# @app.route('/48818187')
+@app.route('/36636186')
+def dog_366():
+    return render_template('dog_36636186.html')
+@app.route('/42904054')
+def dog_429():
+    return render_template('dog_42904054.html')
+@app.route('/43078721')
+def dog_430():
+    return render_template('dog_43078721.html')
+@app.route('/45447002')
+def dog_454():
+    return render_template('dog_45447002.html')
+@app.route('/48818187')
+def dog_488():
+    return render_template('dog_48818187.html')
 # # 5 ids for cat page 
-# @app.route('/51289678')
-# @app.route('/52058185')
-# @app.route('/52072231')
-# @app.route('/52106194')
-# @app.route('/52167059')
+@app.route('/51289678')
+def cat_512():
+    return render_template('cat_51289678.html')
+@app.route('/52058185')
+def cat_520():
+    return render_template('cat_52058185.html')
+@app.route('/52072231')
+def cat_5207():
+    return render_template('cat_52072231.html')
+@app.route('/52106194')
+def cat_521():
+    return render_template('cat_52106194.html')
+@app.route('/52167059')
+def cat_5216():
+    return render_template('cat_52167059.html')
 ###############################################################################
 # main driver function
