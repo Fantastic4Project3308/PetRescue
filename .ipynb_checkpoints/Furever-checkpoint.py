5845b478be747505cef188193ba175817c05a0a5
Brittany Bilotti <brbi1248@colorado.edu>
2023-04-13 20:41:22 +0000

    Sql for cat and dog tables done

--- .ipynb_checkpoints/Furever-checkpoint.py
+++ .ipynb_checkpoints/Furever-checkpoint.py
@@ -1,8 +1,11 @@
-
+#!/usr/bin/python3
 import prefix
-
 from flask import Flask, url_for, make_response, render_template
 from markupsafe import escape
+import sqlite3
+
+conn = sqlite3.connect('Furever.db', check_same_thread=False)
+c= conn.cursor()
 
 # create app to use in this Flask application
 app = Flask(__name__)
@@ -35,36 +38,279 @@ def catpage():
 def dogpage():
     return render_template('DogPage.html')
 # 5 ids for dogs page 
-@app.route('/36636186')
+@app.route('/dog_36636186')
 def dog_366():
-    return render_template('dog_36636186.html')
-@app.route('/42904054')
+    c.execute("select name from Dog")
+    myresult = c.fetchall()
+    name =  myresult[0][0]
+    c.execute("select description from Dog")
+    myresult2 = c.fetchall()
+    description =  myresult2[0][0]
+    c.execute("select breed from Dog")
+    myresult3 = c.fetchall()
+    breed = myresult3[0][0]
+    c.execute("select age from Dog")
+    myresult4 = c.fetchall()
+    age = myresult4[0][0]
+    c.execute("select gender from Dog")
+    myresult5 = c.fetchall()
+    gender = myresult5[0][0]
+    c.execute("select color from Dog")
+    myresult6 = c.fetchall()
+    color = myresult6[0][0]
+    c.execute("select size from Dog")
+    myresult7 = c.fetchall()
+    size = myresult7[0][0]
+    c.execute("select location from Dog")
+    myresult8 = c.fetchall()
+    location = myresult8[0][0]
+    
+    return render_template('dog_36636186.html',name=name, description=description, breed=breed, age=age, gender=gender, color=color,size=size, location=location)
+@app.route('/dog_42904054')
 def dog_429():
-    return render_template('dog_42904054.html')
-@app.route('/43078721')
+    c.execute("select name from Dog")
+    myresult = c.fetchall()
+    name =  myresult[1][0]
+    c.execute("select description from Dog")
+    myresult2 = c.fetchall()
+    description =  myresult2[1][0]
+    c.execute("select breed from Dog")
+    myresult3 = c.fetchall()
+    breed = myresult3[1][0]
+    c.execute("select age from Dog")
+    myresult4 = c.fetchall()
+    age = myresult4[1][0]
+    c.execute("select gender from Dog")
+    myresult5 = c.fetchall()
+    gender = myresult5[1][0]
+    c.execute("select color from Dog")
+    myresult6 = c.fetchall()
+    color = myresult6[1][0]
+    c.execute("select size from Dog")
+    myresult7 = c.fetchall()
+    size = myresult7[1][0]
+    c.execute("select location from Dog")
+    myresult8 = c.fetchall()
+    location = myresult8[1][0]
+    return render_template('dog_42904054.html',name=name, description=description, breed=breed, age=age, gender=gender, color=color,size=size, location=location)
+@app.route('/dog_43078721')
 def dog_430():
-    return render_template('dog_43078721.html')
-@app.route('/45447002')
+    c.execute("select name from Dog")
+    myresult = c.fetchall()
+    name =  myresult[2][0]
+    c.execute("select description from Dog")
+    myresult2 = c.fetchall()
+    description =  myresult2[2][0]
+    c.execute("select breed from Dog")
+    myresult3 = c.fetchall()
+    breed = myresult3[2][0]
+    c.execute("select age from Dog")
+    myresult4 = c.fetchall()
+    age = myresult4[2][0]
+    c.execute("select gender from Dog")
+    myresult5 = c.fetchall()
+    gender = myresult5[2][0]
+    c.execute("select color from Dog")
+    myresult6 = c.fetchall()
+    color = myresult6[2][0]
+    c.execute("select size from Dog")
+    myresult7 = c.fetchall()
+    size = myresult7[2][0]
+    c.execute("select location from Dog")
+    myresult8 = c.fetchall()
+    location = myresult8[2][0]
+    return render_template('dog_43078721.html',name=name, description=description, breed=breed, age=age, gender=gender, color=color,size=size, location=location)
+@app.route('/dog_45447002')
 def dog_454():
-    return render_template('dog_45447002.html')
-@app.route('/48818187')
+    c.execute("select name from Dog")
+    myresult = c.fetchall()
+    name =  myresult[3][0]
+    c.execute("select description from Dog")
+    myresult2 = c.fetchall()
+    description =  myresult2[3][0]
+    c.execute("select breed from Dog")
+    myresult3 = c.fetchall()
+    breed = myresult3[3][0]
+    c.execute("select age from Dog")
+    myresult4 = c.fetchall()
+    age = myresult4[3][0]
+    c.execute("select gender from Dog")
+    myresult5 = c.fetchall()
+    gender = myresult5[3][0]
+    c.execute("select color from Dog")
+    myresult6 = c.fetchall()
+    color = myresult6[3][0]
+    c.execute("select size from Dog")
+    myresult7 = c.fetchall()
+    size = myresult7[3][0]
+    c.execute("select location from Dog")
+    myresult8 = c.fetchall()
+    location = myresult8[3][0]
+    return render_template('dog_45447002.html',name=name, description=description, breed=breed, age=age, gender=gender, color=color,size=size, location=location)
+@app.route('/dog_48818187')
 def dog_488():
-    return render_template('dog_48818187.html')
+    c.execute("select name from Dog")
+    myresult = c.fetchall()
+    name =  myresult[4][0]
+    c.execute("select description from Dog")
+    myresult2 = c.fetchall()
+    description =  myresult2[4][0]
+    c.execute("select breed from Dog")
+    myresult3 = c.fetchall()
+    breed = myresult3[4][0]
+    c.execute("select age from Dog")
+    myresult4 = c.fetchall()
+    age = myresult4[4][0]
+    c.execute("select gender from Dog")
+    myresult5 = c.fetchall()
+    gender = myresult5[4][0]
+    c.execute("select color from Dog")
+    myresult6 = c.fetchall()
+    color = myresult6[4][0]
+    c.execute("select size from Dog")
+    myresult7 = c.fetchall()
+    size = myresult7[4][0]
+    c.execute("select location from Dog")
+    myresult8 = c.fetchall()
+    location = myresult8[4][0]
+    return render_template('dog_48818187.html',name=name, description=description, breed=breed, age=age, gender=gender, color=color,size=size, location=location)
 # # 5 ids for cat page 
-@app.route('/51289678')
+@app.route('/cat_51289678')
 def cat_512():
-    return render_template('cat_51289678.html')
-@app.route('/52058185')
+ 
+    c.execute("select name from Cat")
+    myresult = c.fetchall()
+    name =  myresult[0][0]
+    c.execute("select description from Cat")
+    myresult2 = c.fetchall()
+    description =  myresult2[0][0]
+    c.execute("select breed from Cat")
+    myresult3 = c.fetchall()
+    breed = myresult3[0][0]
+    c.execute("select age from Cat")
+    myresult4 = c.fetchall()
+    age = myresult4[0][0]
+    c.execute("select gender from Cat")
+    myresult5 = c.fetchall()
+    gender = myresult5[0][0]
+    c.execute("select color from Cat")
+    myresult6 = c.fetchall()
+    color = myresult6[0][0]
+    c.execute("select size from Cat")
+    myresult7 = c.fetchall()
+    size = myresult7[0][0]
+    c.execute("select location from Cat")
+    myresult8 = c.fetchall()
+    location = myresult8[0][0]
+    
+    return render_template('cat_51289678.html', name=name, description=description, breed=breed, age=age, gender=gender, color=color,size=size, location=location)
+@app.route('/cat_52058185')
 def cat_520():
-    return render_template('cat_52058185.html')
-@app.route('/52072231')
+    c.execute("select name from Cat")
+    myresult = c.fetchall()
+    name =  myresult[1][0]
+    c.execute("select description from Cat")
+    myresult2 = c.fetchall()
+    description =  myresult2[1][0]
+    c.execute("select breed from Cat")
+    myresult3 = c.fetchall()
+    breed = myresult3[1][0]
+    c.execute("select age from Cat")
+    myresult4 = c.fetchall()
+    age = myresult4[1][0]
+    c.execute("select gender from Cat")
+    myresult5 = c.fetchall()
+    gender = myresult5[1][0]
+    c.execute("select color from Cat")
+    myresult6 = c.fetchall()
+    color = myresult6[1][0]
+    c.execute("select size from Cat")
+    myresult7 = c.fetchall()
+    size = myresult7[1][0]
+    c.execute("select location from Cat")
+    myresult8 = c.fetchall()
+    location = myresult8[1][0]
+    return render_template('cat_52058185.html', name=name, description=description, breed=breed, age=age, gender=gender, color=color,size=size, location=location)
+@app.route('/cat_52072231')
 def cat_5207():
-    return render_template('cat_52072231.html')
-@app.route('/52106194')
+    c.execute("select name from Cat")
+    myresult = c.fetchall()
+    name =  myresult[2][0]
+    c.execute("select description from Cat")
+    myresult2 = c.fetchall()
+    description =  myresult2[2][0]
+    c.execute("select breed from Cat")
+    myresult3 = c.fetchall()
+    breed = myresult3[2][0]
+    c.execute("select age from Cat")
+    myresult4 = c.fetchall()
+    age = myresult4[2][0]
+    c.execute("select gender from Cat")
+    myresult5 = c.fetchall()
+    gender = myresult5[2][0]
+    c.execute("select color from Cat")
+    myresult6 = c.fetchall()
+    color = myresult6[2][0]
+    c.execute("select size from Cat")
+    myresult7 = c.fetchall()
+    size = myresult7[2][0]
+    c.execute("select location from Cat")
+    myresult8 = c.fetchall()
+    location = myresult8[2][0]
+    return render_template('cat_52072231.html',name=name, description=description, breed=breed, age=age, gender=gender, color=color,size=size, location=location)
+@app.route('/cat_52106194')
 def cat_521():
-    return render_template('cat_52106194.html')
-@app.route('/52167059')
+    c.execute("select name from Cat")
+    myresult = c.fetchall()
+    name =  myresult[3][0]
+    c.execute("select description from Cat")
+    myresult2 = c.fetchall()
+    description =  myresult2[3][0]
+    c.execute("select breed from Cat")
+    myresult3 = c.fetchall()
+    breed = myresult3[3][0]
+    c.execute("select age from Cat")
+    myresult4 = c.fetchall()
+    age = myresult4[3][0]
+    c.execute("select gender from Cat")
+    myresult5 = c.fetchall()
+    gender = myresult5[3][0]
+    c.execute("select color from Cat")
+    myresult6 = c.fetchall()
+    color = myresult6[3][0]
+    c.execute("select size from Cat")
+    myresult7 = c.fetchall()
+    size = myresult7[3][0]
+    c.execute("select location from Cat")
+    myresult8 = c.fetchall()
+    location = myresult8[3][0]
+    return render_template('cat_52106194.html',name=name, description=description, breed=breed, age=age, gender=gender, color=color,size=size, location=location)
+@app.route('/cat_52167059')
 def cat_5216():
-    return render_template('cat_52167059.html')
+    c.execute("select name from Cat")
+    myresult = c.fetchall()
+    name =  myresult[4][0]
+    c.execute("select description from Cat")
+    myresult2 = c.fetchall()
+    description =  myresult2[4][0]
+    c.execute("select breed from Cat")
+    myresult3 = c.fetchall()
+    breed = myresult3[4][0]
+    c.execute("select age from Cat")
+    myresult4 = c.fetchall()
+    age = myresult4[4][0]
+    c.execute("select gender from Cat")
+    myresult5 = c.fetchall()
+    gender = myresult5[4][0]
+    c.execute("select color from Cat")
+    myresult6 = c.fetchall()
+    color = myresult6[4][0]
+    c.execute("select size from Cat")
+    myresult7 = c.fetchall()
+    size = myresult7[4][0]
+    c.execute("select location from Cat")
+    myresult8 = c.fetchall()
+    location = myresult8[4][0]
+    return render_template('cat_52167059.html', name=name, description=description, breed=breed, age=age, gender=gender, color=color,size=size, location=location)
 ###############################################################################
 # main driver function
--- .ipynb_checkpoints/sql-checkpoint.py
+++ .ipynb_checkpoints/sql-checkpoint.py
@@ -1,12 +1,24 @@
 #!/usr/bin/python3
 import sqlite3
 
+#connecting to database
 conn = sqlite3.connect('Furever.db')
 c= conn.cursor()
 
+#creating cat table
 c.execute("DROP TABLE IF EXISTS Cat")
 c.execute("Create table Cat(id int, name varchar(45), description varchar(1000), breed varchar(20), age varchar(50), gender varchar(10),color varchar(10),size varchar(10), location varchar(10), primary key (id));")
 
+#creating dog table
+c.execute("DROP TABLE IF EXISTS Dog")
+c.execute("Create table Dog(id int, name varchar(45), description varchar(1000), breed varchar(20), age varchar(50), gender varchar(10),color varchar(10),size varchar(10), location varchar(10), primary key (id));")
+
+#creating adoption form table
+c.execute("DROP TABLE IF EXISTS adoption")
+c.execute("Create table adoption(name varchar(45), occupation varchar(45), address varchar(100), duration varchar(50), phone varchar(15),email varchar(50));")
+
+
+#creating dictionaries of all the cat information 
 cat_51289679_dict = {
     'id':51289679,
     'name': 'Mocha',
@@ -18,14 +30,154 @@ cat_51289679_dict = {
     'size': 'Medium',
     'location': 'Foster Care'
 }
+cat_52058185_dict = {
+    'id' : 52058185,
+    'name': 'Peppi',
+'description': "Like peanut butter and jelly, cups and saucers, Kermit and Miss Piggy, some things are meant to be together. And that's especially true for Peppi and Stanley. This adorable mom and son pair were brought to SAS together, and were briefly separated, but when staff saw how much they missed each other, they were reunited at their foster parent's home. Peppi didn't eat while they were apart. When Stanley saw Peppi again, he mewed happily for the first time! They were so happy to be back together. Now they sleep together like magnets and Stanley follows his mom devotedly wherever she goes.",
+'breed':'Domestic Medium Hair/Mix',
+'age':'4 years 5 days',
+'gender':'Female',
+'color':'Orange/Black',
+'size':'Medium',
+'location':'Foster Care' }
+cat_3_dict = {
+    'id' : 52072231,
+    'name': 'Stanley',
+'description': "Like peanut butter and jelly, cups and saucers, Kermit and Miss Piggy, some things are meant to be together. And that's especially true for Peppi and Stanley. This adorable mom and son pair were brought to SAS together, and were briefly separated, but when staff saw how much they missed each other, they were reunited at their foster parent's home. Peppi didn't eat while they were apart. When Stanley saw Peppi again, he mewed happily for the first time! They were so happy to be back together. Now they sleep together like magnets and Stanley follows his mom devotedly wherever she goes.",
+'breed':'Domestic Longhair/Mix',
+'age':'2 years 5 days',
+'gender':'Male',
+'color':'Orange',
+'size':'Medium',
+'location':'Foster Care'
+}
+cat_4_dict = {
+    'id' : 52106194,
+    'name': 'Lily',
+'description': "Hi! I'm a sweet, timid kitty who loves head scratches and Churu treats! Once I feel comfortable with you, I'm inquisitive and playful. Toys with strings and feathers are my favorite. I also enjoy snuggling in blankets, giving head bumps and bird watching. I have very polite litter box manners. When I'm not curled up next to you, I'll be napping nearby because I like the company.",
+    'breed':'Domestic Medium Hair/Mix',
+'age':'2 years',
+'gender':'Female',
+'color':'Brown/white',
+'size':'Small',
+'location':'Foster Care'
+}
+cat_5_dict = {
+    'id' : 52167059,
+    'name': 'Boris',
+'description': "Boris is a gentle cat who true to his breed is a chatty boy.",
+    'breed':'Siamese/Mix',
+'age':'6 years 12 days',
+'gender':'Male',
+'color':'Seal',
+'size':'Large',
+'location':'Cat Free Roaming Room'
+}
 
-cat = cat_51289679_dict
+#creating dictionaries of all the dog information
+dog1_dict = {
+    'id':36636186,
+    'name': 'Mojo',
+    'description': "He is a perfect dog in many ways. He can be left alone with the roam of the house and just sleeps. He only chews on his toys. He's potty trained. He's a hilarious home companion.  He has no resource guarding and is great with body handling, he just loves any attention!  He's smart and can be desensitized to triggers like vacuums and blenders with steady positive association.  He may not have had socialization as a puppy to normal household things like this.",
+    'breed': 'Terrier, American Pit Bull/Mix',
+    'age': '6 years 7 months 7 days',
+    'gender': 'Male',
+    'color': 'Brindle/White',
+    'size': 'Medium',
+    'location': 'Foster Care'
+}
+dog2_dict = {
+    'id':42904054,
+    'name': 'Jane',
+    'description': "I'm a Golden Girl in dog form: sassy, sprightly, and a card-carrying member of the AARP. An athletic and active gal, my hobbies include food puzzles, enjoying belly rubs, and tap dancing! I need a kind adopter to make me feel safe and loved for the rest of my years: someone to appreciate my marvelous, unique qualities. I'm a resilient, physically strong, sweetie-pie: well-mannered indoors, and don't require long walks, but would be happiest with a quiet yard of my own. No humans under 12 years, or kitties or doggos please.",
+    'breed': 'Terrier,Pit Bull/Mix',
+    'age': '10 years 8 months 22 days',
+    'gender': 'Female',
+    'color': 'Brindle',
+    'size': 'Large',
+    'location': 'Foster Care'
+}
+dog3_dict = {
+    'id':43078721,
+    'name': 'Ra',
+    'description': "Ra is a spirited and smart boy with loads of personality. He's looking for the perfect forever home with the right balance of love, training and structure to help him continue to blossom.",
+    'breed': 'Terrier,American Pit Bull/Mix',
+    'age': '4 years 4 months 4 days',
+    'gender': 'Male',
+    'color': 'Brown/white',
+    'size': 'Medium',
+    'location': 'Foster Care'
+}
+dog4_dict = {
+    'id':45447002,
+    'name': 'Puck',
+    'description': "Hi, I'm Puck, and like most everyone, I have some quirks but that just makes me more interesting. My foster mom says I have lots of nuance... If you don't know what a Belgian Malinois is, you should read up about us - we are known for loyalty, herding and protection. Me personally, well I'm almost five years old, and have often been told that I'm a handsome boy with my shiny brindle coat and expressive ears. I'm also a fast sprinter with quick reflexes and I love to hunt for small critters outside and chase soccer balls!",
+    'breed': 'Terrier, American Pit Bull/Mix',
+    'age': '5 years 5 months 8 days',
+    'gender': 'Male',
+    'color': 'Brindle/white',
+    'size': 'Large',
+    'location': 'Foster Care'
+}
+dog5_dict = {
+    'id':48818187,
+    'name': 'Pickles',
+    'description': "Pickles didn't have the best start to his life...he came to us from a situation where he was not consistently treated with love and kindness. It takes a little bit of time for him to warm up and trust, but once you're in he is totally in love with you! He spent some time in a foster home, and his foster parent told us Pickles was so sweet and loving. He was an incredibly polite houseguest! His favorite nap spots were on the couch or on the floor next to it with his people nearby for some petting and affection. In the morning he likes to run his zoomies out and is always up for a game of ball in the yard. He's super athletic and very fast! He walks fairly well on leash, but will pull sometimes. The good news is that he's treat motivated and is a quick study, so he's ready to learn to have better leash manners.",
+    'breed':'Terrier, Pit Bull/Mix',
+    'age': '3 years 3 months 17 days',
+    'gender': 'Male',
+    'color': 'Brindle/white',
+    'size': 'Large',
+    'location': 'Dog Kennels'
+}
+
+
+#inserting dictionary information into cat tables
 c.execute("INSERT INTO Cat (id, name, description, breed, age, gender, color, size, location) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
-          (cat['id'], cat['name'], cat['description'], cat['breed'], cat['age'], cat['gender'], cat['color'], cat['size'], cat['location']))
+          (cat_51289679_dict['id'], cat_51289679_dict['name'], cat_51289679_dict['description'], cat_51289679_dict['breed'], cat_51289679_dict['age'], cat_51289679_dict['gender'], cat_51289679_dict['color'], cat_51289679_dict['size'], cat_51289679_dict['location']))
+
+c.execute("INSERT INTO Cat (id, name, description, breed, age, gender, color, size, location) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+          (cat_52058185_dict['id'], cat_52058185_dict['name'], cat_52058185_dict['description'], cat_52058185_dict['breed'], cat_52058185_dict['age'], cat_52058185_dict['gender'], cat_52058185_dict['color'], cat_52058185_dict['size'], cat_52058185_dict['location']))
+
+c.execute("INSERT INTO Cat (id, name, description, breed, age, gender, color, size, location) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+          (cat_3_dict['id'], cat_3_dict['name'], cat_3_dict['description'], cat_3_dict['breed'], cat_3_dict['age'], cat_3_dict['gender'], cat_3_dict['color'], cat_3_dict['size'], cat_3_dict['location']))
+
+c.execute("INSERT INTO Cat (id, name, description, breed, age, gender, color, size, location) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+          (cat_4_dict['id'], cat_4_dict['name'], cat_4_dict['description'], cat_4_dict['breed'], cat_4_dict['age'], cat_4_dict['gender'], cat_4_dict['color'], cat_4_dict['size'], cat_4_dict['location']))
+
+c.execute("INSERT INTO Cat (id, name, description, breed, age, gender, color, size, location) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+          (cat_5_dict['id'], cat_5_dict['name'], cat_5_dict['description'], cat_5_dict['breed'], cat_5_dict['age'], cat_5_dict['gender'], cat_5_dict['color'], cat_5_dict['size'], cat_5_dict['location']))
 
 
+#inserting dictionary information into dog tables
+c.execute("INSERT INTO Dog (id, name, description, breed, age, gender, color, size, location) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+          (dog1_dict['id'], dog1_dict['name'], dog1_dict['description'], dog1_dict['breed'], dog1_dict['age'], dog1_dict['gender'], dog1_dict['color'], dog1_dict['size'], dog1_dict['location']))
+
+c.execute("INSERT INTO Dog (id, name, description, breed, age, gender, color, size, location) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+          (dog2_dict['id'], dog2_dict['name'], dog2_dict['description'], dog2_dict['breed'], dog2_dict['age'], dog2_dict['gender'], dog2_dict['color'], dog2_dict['size'], dog2_dict['location']))
+
+c.execute("INSERT INTO Dog (id, name, description, breed, age, gender, color, size, location) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+          (dog3_dict['id'], dog3_dict['name'], dog3_dict['description'], dog3_dict['breed'], dog3_dict['age'], dog3_dict['gender'], dog3_dict['color'], dog3_dict['size'], dog3_dict['location']))
+
+c.execute("INSERT INTO Dog (id, name, description, breed, age, gender, color, size, location) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+          (dog4_dict['id'], dog4_dict['name'], dog4_dict['description'], dog4_dict['breed'], dog4_dict['age'], dog4_dict['gender'], dog4_dict['color'], dog4_dict['size'], dog4_dict['location']))
+
+c.execute("INSERT INTO Dog (id, name, description, breed, age, gender, color, size, location) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+          (dog5_dict['id'], dog5_dict['name'], dog5_dict['description'], dog5_dict['breed'], dog5_dict['age'], dog5_dict['gender'], dog5_dict['color'], dog5_dict['size'], dog5_dict['location']))
+
+
+#to go into test file
 try:
     c.execute("select * from Cat")
+    myresult = c.fetchall()
+    for x in myresult:
+        print(x)
+except:
+    print("Error")
+conn.commit()
+
+try:
+    c.execute("select * from Dog")
     myresult = c.fetchall()
     for x in myresult:
         print(x)
